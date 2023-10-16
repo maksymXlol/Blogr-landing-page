dae660dc3bb97f9fc068fe7a207a635c3bae4612
maksymXlol <124809697+maksymXlol@users.noreply.github.com>
2023-10-16 15:26:51 +0100

    hw due 16/11/23

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-let burgerMenu = document.querySelector(".burger-btn");
-let dropList = document.querySelector(".droplist");
-burgerMenu.addEventListener("click", click);
-function click() {
-  dropList.classList.toggle("visible");
-}
+// let burgerMenu = document.querySelector(".burger-btn");
+// let dropList = document.querySelector(".droplist");
+// burgerMenu.addEventListener("click", click);
+// function click() {
+//   dropList.classList.toggle("visible");
+// }
+
+document.addEventListener("click", (event) => {
+  let dropdownBtn = event.target.closest("[data-dropdown-btn]");
+
+  if (dropdownBtn) {
+    let dropdownTarget = dropdownBtn.getAttribute("data-dropdown-target");
+
+    let targetElement = document.querySelector(
+      `[data-dropdown-id=${dropdownTarget}]`
+    );
+    let targetOpen =
+      targetElement.getAttribute("data-dropdown-open") === "true"
+        ? "false"
+        : "true";
+    targetElement.setAttribute("data-dropdown-open", targetOpen);
+    dropdownBtn.setAttribute("data-dropdown-open", targetOpen);
+  }
+});
